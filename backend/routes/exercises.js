e1fabb85c1f381b70ba00b588f6511d545b5cba5
Kai Jie Chow <kj1chow@gmail.com>
2021-03-24 15:28:01 +0000

    API postman working

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -3,7 +3,7 @@ let Exercise = require('../models/exercise.model');
 
 router.route('/').get((req, res) => {
     Exercise.find()
-        .then(exercise => res.json(exercise))
+        .then(exercises => res.json(exercises))
         .catch(err =>  res.status(400).json('Error ' + err));
 });
 
